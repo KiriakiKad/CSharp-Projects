5ad0ed501a42c1f941a69a8ea5168deda13fa45f
KiriakiKad <kiriakikadrefi@gmail.com>
2025-07-29 17:42:44 +0300

    fileInfo commit

---
+++ CSharp-Begginer-Projects/FileInfo/Count_the_words/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_the_words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"......\.....\...\myFile.txt";
+
+            var fileInfo = new FileInfo(path);
+
+            if (fileInfo.Exists)
+            {
+                var input = File.ReadAllText(path);
+                var words = input.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                var wordCount = words.Length;
+                Console.Write("Total words: " + wordCount);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ CSharp-Begginer-Projects/FileInfo/Find_the_longest_word/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_longest_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"...\...\...\myFile.txt";
+
+            var file = new FileInfo(path);
+
+            if (file.Exists)
+            {
+                var longestWord = GetLongestWord(file);
+                Console.WriteLine("Longest word: " + longestWord);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        public static string GetLongestWord(FileInfo file)
+        {
+            var text = File.ReadAllText(file.FullName);
+
+            var words = text.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            var max = -1;
+            var longestWord = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > max)
+                {
+                    max = words[i].Length;
+                    longestWord = words[i];
+                }
+            }
+            return longestWord;
+        }
+    }
+}
