cea73127db6637d992c535f5143990e78cc59f45
KiriakiKad <kiriakikadrefi@gmail.com>
2025-09-03 20:53:47 +0300

    Add DelegatesCalculator project

---
+++ CSharp-Advanced-Projects/DelegatesCalculator/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesCalculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"Add: {a} + {b} = {result}");
+            return result;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine($"Subtract: {a} - {b} = {result}");
+            return result;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine($"Multiply: {a} * {b} = {result}");
+            return result;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero!");
+            int result = a / b;
+            Console.WriteLine($"Divide: {a} / {b} = {result}");
+            return result;
+        }
+    }
+}
---
+++ CSharp-Advanced-Projects/DelegatesCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesCalculator
+{
+    public delegate int Operation(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+
+            Operation op = calc.Add;
+            op += calc.Subtract;
+            op += calc.Multiply;
+            op += calc.Divide;
+
+            op(10, 5);
+
+        }
+    }
+}
