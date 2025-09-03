38721a45fe6886f5e85009268d123fc0be455b14
KiriakiKad <kiriakikadrefi@gmail.com>
2025-09-03 20:27:25 +0300

    Add Generics Project

---
+++ CSharp-Advanced-Projects/GenericsExercise/GenericList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExercise
+{
+    public class GenericList<T>
+    {
+        private T[] _items;
+        private int _count { get; set; }
+
+        public GenericList()
+        {
+            _items =new T[10];
+            _count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if(_count == _items.Length)
+            {
+                throw new InvalidOperationException("List is full");
+            }
+            _items[_count] = item;
+            _count++;
+        }
+
+        public T Get(int index)
+        {
+            if(index < 0 || index >= _count)
+            {
+                throw new ArgumentOutOfRangeException("Index out of range");
+            }
+            return _items[index];
+        }
+
+        
+    }
+}
---
+++ CSharp-Advanced-Projects/GenericsExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intList = new GenericList<int>();
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+            Console.WriteLine(intList.Get(0)); // Output: 1
+            Console.WriteLine(intList.Get(1)); // Output: 2
+            Console.WriteLine(intList.Get(2)); // Output: 3
+            GenericList<string> stringList = new GenericList<string>();
+            stringList.Add("Hello");
+            stringList.Add("World");
+            Console.WriteLine(stringList.Get(0)); // Output: Hello
+            Console.WriteLine(stringList.Get(1)); // Output: World
+        }
+    }
+}
