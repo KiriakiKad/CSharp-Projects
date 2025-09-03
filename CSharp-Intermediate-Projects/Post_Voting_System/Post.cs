2f851623a844aba8688009061760b0fad58d557e
KiriakiKad <kiriakikadrefi@gmail.com>
2025-09-03 13:34:17 +0300

    Add PostVotingSystem project

---
+++ CSharp-Intermediate-Projects/Post_Voting_System/Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_Voting_System
+{
+    public class Post
+    {
+        private string _title; 
+        private string _description; 
+        private DateTime _dateCreated; 
+        private int _votes; 
+        
+        public Post(string title, string description) {
+            _title = title; 
+            _description = description; 
+            _dateCreated = DateTime.Now; 
+            _votes = 0; 
+        }
+
+        public void UpVoting() {
+            _votes++; 
+        }
+        public void DownVoting() {
+
+            if (_votes > 0) 
+                _votes--; 
+            else 
+                Console.WriteLine("Votes cannot be negative."); 
+        }
+        public int GetVotes() {
+            return _votes; 
+        }
+    }
+}
---
+++ CSharp-Intermediate-Projects/Post_Voting_System/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_Voting_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("My First Post", "This is the description of my first post."); 
+            Voting(post);
+        }
+        public static void Voting(Post post) {
+
+            Console.WriteLine("1 NEGATIVE VOTE: "); 
+            post.DownVoting(); 
+            Console.WriteLine("1 POSITIVE VOTE: "); 
+            post.UpVoting(); 
+            Console.WriteLine("1 POSITIVE VOTE: "); 
+            post.UpVoting(); 
+            Console.WriteLine("1 POSITIVE VOTE: "); 
+            post.UpVoting(); 
+            Console.WriteLine("1 NEGATIVE VOTE: "); 
+            post.DownVoting(); 
+            Console.WriteLine("Votes: " + post.GetVotes()); 
+        }
+    }
+}
