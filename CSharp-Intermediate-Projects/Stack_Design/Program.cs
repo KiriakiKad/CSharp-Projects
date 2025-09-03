cb6490fbe3bc9b0e51e506209146e81fe055b9c4
KiriakiKad <kiriakikadrefi@gmail.com>
2025-09-03 13:03:53 +0300

    Add StackDesign project

---
+++ CSharp-Intermediate-Projects/Stack_Design/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+
+            stack.Push(2);
+            stack.Push(3);
+
+            stack.Push("hello");
+            stack.Push(DateTime.Now);
+
+            try
+            {
+                stack.Push(null);          // Push with null
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("EXCEPTION: " + ex.Message);
+            }
+
+            stack.Pop();
+            stack.Pop();
+
+
+            stack.Clear();
+        }
+    }
+}
---
+++ CSharp-Intermediate-Projects/Stack_Design/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Design
+{
+    public class Stack
+    {
+        private List<object> _stack;
+
+        public Stack()
+        {
+            _stack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+
+            if (obj == null)
+                throw new InvalidOperationException("obj cannot be null");
+
+            _stack.Add(obj);
+            Console.WriteLine("ADDED: " + obj.ToString());
+        }
+
+        public object Pop()
+        {
+
+            if (_stack.Count == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            var obj = _stack[_stack.Count - 1];
+            _stack.RemoveAt(_stack.Count - 1);
+            Console.WriteLine("POPPED: " + obj.ToString());
+            return obj;
+        }
+
+        public void Clear()
+        {
+
+            _stack.Clear();
+        }
+    }
+}
