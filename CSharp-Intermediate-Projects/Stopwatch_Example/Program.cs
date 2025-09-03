9a1d7237b54dfb1869f3c146ea73110769793190
KiriakiKad <kiriakikadrefi@gmail.com>
2025-09-03 13:25:20 +0300

    Add Stopwatch project

---
+++ CSharp-Intermediate-Projects/Stopwatch_Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Calculate(stopwatch);
+        }
+        public static void Calculate(Stopwatch stopwatch)
+        {
+            try
+            {
+                stopwatch.Start(); // Simulate some work
+                System.Threading.Thread.Sleep(2000);
+                var duration = stopwatch.Stop();
+
+                Console.WriteLine("Elapsed time: " + duration.TotalSeconds + " seconds");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ CSharp-Intermediate-Projects/Stopwatch_Example/Stopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch_Example
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime; 
+        private DateTime _endTime; 
+        private bool _flag; 
+        
+        public void Start() { 
+            if (_flag) 
+                throw new InvalidOperationException("Stopwatch is already running."); 
+            
+            _startTime = DateTime.Now; 
+            _flag = true;
+        }
+        public TimeSpan Stop() {
+            if (!_flag) 
+                throw new InvalidOperationException("Stopwatch is not running."); 
+            
+            _endTime = DateTime.Now; 
+            _flag = false; 
+            
+            return _endTime - _startTime; 
+        }
+    }
+}
