a32ea3d61f6dcf01a053da170bb6becc6dc8fe9f
KiriakiKad <kiriakikadrefi@gmail.com>
2025-09-10 13:42:10 +0300

    Add DynamicDemo project

---
+++ CSharp-Advanced-Projects/DynamicDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic value = 100;
+            Console.WriteLine("The value is: "  + value);
+
+            value = "Hello, World!";
+            Console.WriteLine("The value is: " + value);
+
+            value = DateTime.Now;
+            Console.WriteLine("The value is: " + value);
+
+            //Exception at runtime
+            Console.WriteLine("Non exidted method: " + value.SayHello());
+        }
+    }
+}
